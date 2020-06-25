6ea4c52586bce3929ac9f6d865266ed17f7a94a9
Jonathan Giannuzzi <jonathan@giannuzzi.me>
2020-06-25 12:41:16 +0200

    Initial commit

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "tls" {
+  version = "~> 2.1"
+}
+provider "null" {
+  version = "~> 2.1"
+}
+
+resource "tls_private_key" "key" {
+  algorithm = "ECDSA"
+}
+
+resource "null_resource" "display" {
+  triggers = {
+    pubkey  = tls_private_key.key.public_key_pem
+    privkey = tls_private_key.key.private_key_pem
+  }
+}
