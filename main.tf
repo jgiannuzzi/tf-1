f8b12e9ab6910a6d38371deb19044b4c5c6367d1
Jonathan Giannuzzi <jgiannuzzi@users.noreply.github.com>
2020-09-15 18:53:23 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "tls_private_key" "key" {
   algorithm = "ECDSA"
 }
 
-resource "null_resource" "display" {
+resource "null_resource" "display2" {
   triggers = {
     pubkey  = tls_private_key.key.public_key_pem
     privkey = tls_private_key.key.private_key_pem
